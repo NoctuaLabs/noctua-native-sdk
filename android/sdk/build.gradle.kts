09ebc528bfaede7cf1dcca456cebdf7706755251
Wijaya Adhi Surya <wijaya.adhisurya@gmail.com>
2024-10-08 15:48:17 +0700

    feat: add AccountRepository to sdk

--- android/sdk/build.gradle.kts
+++ android/sdk/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     `maven-publish`
     id("com.vanniktech.maven.publish") version "0.29.0"
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -43,6 +44,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
     }
 }
 
@@ -116,7 +120,18 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.firebase.analytics)
     implementation(libs.facebook.core)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
