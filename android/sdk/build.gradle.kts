1927993340464fe7a5a7ee4ae83a40283adc6963
Wijaya Adhi Surya <wijaya.adhisurya@gmail.com>
2024-10-15 11:12:39 +0700

    fix: remove unneeded kotlin compose libs

--- android/sdk/build.gradle.kts
+++ android/sdk/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     `maven-publish`
     id("com.vanniktech.maven.publish") version "0.29.0"
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -46,7 +45,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose = true
+        compose = false
     }
 }
 
@@ -120,18 +119,5 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.firebase.analytics)
     implementation(libs.facebook.core)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
