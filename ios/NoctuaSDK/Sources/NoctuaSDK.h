6cd162a31ec5650ea67775af11fbb09a65388fdf
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-10-27 16:02:05 +0700

    feat: bump version

--- ios/NoctuaSDK/Sources/NoctuaSDK.h
+++ ios/NoctuaSDK/Sources/NoctuaSDK.h
@@ -6,5 +6,5 @@ FOUNDATION_EXPORT double NoctuaSDKVersionNumber;
 //! Project version string for NoctuaSDK.
 FOUNDATION_EXPORT const unsigned char NoctuaSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <NoctuaSDK/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <NoctuaSDK/PublicHeader.h> 
 
