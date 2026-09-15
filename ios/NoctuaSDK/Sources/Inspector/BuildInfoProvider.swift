af1a95e149889fb543e90aa4cc0ab67df9d46536
Noctua Gitlab CI <gitlab-ci@noctua.gg>
2026-09-15 10:50:07 +0700

    bump version to 0.40.1 [skip ci]

--- ios/NoctuaSDK/Sources/Inspector/BuildInfoProvider.swift
+++ ios/NoctuaSDK/Sources/Inspector/BuildInfoProvider.swift
@@ -13,7 +13,7 @@ import Foundation
 public enum BuildInfoProvider {
 
     /// SDK semver. Bumped in lockstep with the podspec.
-    public static let nativeSdkVersion: String = "0.40.0"
+    public static let nativeSdkVersion: String = "0.40.1"
 
     /// Reads `PROJECT_ID` from the bundled `GoogleService-Info.plist`.
     /// Returns "" when the plist isn't bundled (Firebase not configured).
